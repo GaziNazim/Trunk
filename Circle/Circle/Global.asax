0ad3c8e93240428371470bb435e66353d4c0a3e7
unknown <gazinazim1@gmail.com>
2017-06-11 23:58:00 +0600

    First Grand Commit

---
+++ Circle/Circle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Circle.MvcApplication" Language="C#" %>
---
+++ Circle/Circle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Circle.Startup))]
+namespace Circle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
